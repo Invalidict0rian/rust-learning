12b2eb0aa055d6a124af966362bcfe9248f5d3a1
Invalidict0rian <104943463+Invalidict0rian@users.noreply.github.com>
2026-02-22 16:19:56 -0600

    finished guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,12 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
+    let answer: u32 = rand::thread_rng().gen_range(1..=100);
+    println!("answer is : {answer}");
+
+    loop{
     println!("Guess a Number Any Number: ");
 
     let mut guess = String::new();
@@ -9,5 +15,17 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Failed to Parse Guess");
+
     println!("you guessed {guess}");
+
+    match guess.cmp(&answer){
+        Ordering::Less => println!("guess too small"),
+        Ordering::Equal => {
+            println!("Hooray You Win!");
+            break;
+            },
+        Ordering::Greater => println!("guess too big"),
+    }
+}
 }
